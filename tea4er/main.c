0dedf9714c4091a23aea09280b0cfee65cf8fd9e
gitPeregrin <136462295+gitPeregrin@users.noreply.github.com>
2023-06-13 15:41:13 +0300

    Initial Commit

---
+++ tea4er/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tea4er
+//
+//  Created by Mike An on 13.06.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
